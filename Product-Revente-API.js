4ba09f0559ddc907494a2c54b97347649e59ae6b
Gabriel-Tache <gabriel.tache@edu.ece.fr>
2025-03-26 20:10:13 +0100

    Update Product-Revente-API.js

--- Product-Revente-API.js
+++ Product-Revente-API.js
@@ -15,7 +15,8 @@
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/estimate", {
+      const response = await fetch("https://api-estimation.onrender.com/estimate", {
+
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
